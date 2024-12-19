3386a2078395428f700af492954e131782a4079d
TERMZL0ckd0wn <128064869+TERMZL0ckd0wn@users.noreply.github.com>
2024-12-19 09:41:03 +0800

    Add files via upload

---
+++ bm/BLEASp2/BLEASp2.ino
@@ -0,0 +1,103 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+int babySeatStat = 13;
+int babySeat = 0;
+
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "b5d82e63-2ad1-4b94-aa9e-3c42b325fad8"
+#define CHARACTERISTIC_UUID "72f0621e-2b1a-4359-a6cd-57b62b5fbb16"
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Read pins
+  pinMode(babySeatStat, INPUT);
+
+  // Create the BLE Device
+  BLEDevice::init("BabyLESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+    
+    babySeat = digitalRead(babySeatStat);
+
+    if (babySeat == HIGH) {
+      std::string alrtmssg = "Baby Warn!";
+      pCharacteristic->setValue(alrtmssg);
+      pCharacteristic->notify();
+      delay(1000);
+    }
+    if (babySeat == LOW) {
+      std::string nonalrt = "Everything OK!";
+      pCharacteristic->setValue(nonalrt);
+      pCharacteristic->notify();
+      delay(1000);
+    }
+
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+}
